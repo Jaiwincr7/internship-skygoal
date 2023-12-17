07a01138155b1b7284f744ca4128a8dbdcc5df72
Jaiwincr7 <chaudharijr@rknec.edu>
2023-12-17 14:25:34 +0530

    1st

---
+++ script.js
@@ -0,0 +1,61 @@
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
+  let counter = 0;
+
+const counterValue = document.getElementById('counter-value');
+const incrementBtn = document.getElementById('increment-btn');
+const decrementBtn = document.getElementById('decrement-btn');
+const resetBtn = document.querySelector('#reset');
+
+// To increment the value of counter
+incrementBtn.addEventListener('click', () => {
+	counter++;
+	counterValue.innerHTML = counter;
+});
+
+// To decrement the value of counter
+decrementBtn.addEventListener('click', () => {
+	counter--;
+	counterValue.innerHTML = counter;
+});
+
+// To reset the counter to zero
+resetBtn.addEventListener('click', reset);
+
+function reset() {
+	counter = 0;
+	counterValue.innerHTML = counter;
+}
+  
